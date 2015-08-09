85a8ba2415d6d1576579099a2aadde2a9f7b56bd
tu-jian <13585722843@126.com>
2015-08-09 18:58:13 +0800

    remove unused code in printf.c
    Signed-off-by: tu-jian <13585722843@126.com>

--- 7_variadic_func/printf.c
+++ 7_variadic_func/printf.c
@@ -1,22 +1,44 @@
 #include <stdio.h>
 #include <stdarg.h>
-#include "print.h"
 
-int main(void)
+void printch(char ch)
 {
-	print("print: %c\n", 'c');
-	print("print: %d\n", 1234567);
-	print("print: %f\n", 1234567.1234567);
-	print("print: %s\n", "string test");
-	print("print: %b\n", 0x12345ff);
-	print("print: %x\n", 0xabcdef);
-	print("print: %%\n");
-	return 0;
+//	console_print(ch);
+	putchar(ch);
+}
+
+void printdec(int dec)
+{
+	if(dec==0) {
+		return;
+	}
+	printdec(dec/10);
+	printch( (char)(dec%10 + '0'));
+}
+
+void printstr(char* str)
+{
+	while(*str) {
+		printch(*str++);
+	}
+}
+
+void printhex(int hex)
+{
+	if(hex==0) {
+		printstr("0x");
+		return;
+	}
+	printhex(hex/16);
+	if(hex < 10) {
+		printch((char)(hex%16 + '0'));
+	} else {
+		printch((char)(hex%16 - 10 + 'a' ));
+	}
 }
 
 void print(char* fmt, ...)
 {
-	double vargflt = 0;
 	int  vargint = 0;
 	char* vargpch = NULL;
 	char vargch = 0;
@@ -37,27 +59,14 @@ void print(char* fmt, ...)
 				printch(vargch);
 				break;
 			case 'd':
-			case 'i':
 				vargint = va_arg(vp, int);
 				printdec(vargint);
-				break;
-			case 'f':
-				vargflt = va_arg(vp, double);
-				/*    va_arg(ap, type), if type is narrow type (char, short, float) an error is given in strict ANSI
-				mode, or a warning otherwise.In non-strict ANSI mode, 'type' is allowed to be any expression. */
-				printflt(vargflt);
 				break;
 			case 's':
 				vargpch = va_arg(vp, char*);
 				printstr(vargpch);
 				break;
-			case 'b':
-			case 'B':
-				vargint = va_arg(vp, int);
-				printbin(vargint);
-				break;
 			case 'x':
-			case 'X':
 				vargint = va_arg(vp, int);
 				printhex(vargint);
 				break;
@@ -75,60 +84,12 @@ void print(char* fmt, ...)
 	va_end(vp);
 }
 
-void printch(char ch)
+int main(void)
 {
-	console_print(ch);
-}
-
-void printdec(int dec)
-{
-	if(dec==0) {
-		return;
-	}
-	printdec(dec/10);
-	printch( (char)(dec%10 + '0'));
-}
-
-void printflt(double flt)
-{
-	int icnt = 0;
-	int tmpint = 0;
-
-	tmpint = (int)flt;
-	printdec(tmpint);
-	printch('.');
-	flt = flt - tmpint;
-	tmpint = (int)(flt * 1000000);
-	printdec(tmpint);
-}
-
-void printstr(char* str)
-{
-	while(*str) {
-		printch(*str++);
-	}
-}
-
-void printbin(int bin)
-{
-	if(bin == 0) {
-		printstr("0b");
-		return;
-	}
-	printbin(bin/2);
-	printch( (char)(bin%2 + '0'));
-}
-
-void printhex(int hex)
-{
-	if(hex==0) {
-		printstr("0x");
-		return;
-	}
-	printhex(hex/16);
-	if(hex < 10) {
-		printch((char)(hex%16 + '0'));
-	} else {
-		printch((char)(hex%16 - 10 + 'a' ));
-	}
+	print("print: %c\n", 'c');
+	print("print: %d\n", 1234567);
+	print("print: %s\n", "string test");
+	print("print: %x\n", 0xabcdef);
+	print("print: %%\n");
+	return 0;
 }
